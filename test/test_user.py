e6e7c32f6f59ef5109ebfe89b731701302661181
deep132raj <deep@ai4mtech.com>
2024-01-04 23:08:51 +0530

    lastest update

--- test/test_user.py
+++ test/test_user.py
@@ -6,6 +6,20 @@ from app.schemas import UserOut
 from .database import client, session
 
 
+@pytest.fixture
+def test_user(client):
+    user_data = {'email': 'test1@example.com', 'password': '123456'}
+    res = client.post('/user', json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+    
+
+def test_login_user(client, test_user):
+    res = client.post('/login', data={'username': test_user['email'], 'password': test_user['password']})
+    assert res.status_code == 200    
+
 def test_root(client):
     res = client.get('/')
     assert res.status_code == 200
@@ -16,6 +30,3 @@ def test_create_user(client):
     UserOut(**res.json())
     assert res.status_code == 201
 
-def test_login_user(client):
-    res = client.post('/login', data={'username': 'test@example.com', 'password': '123456'})
-    assert res.status_code == 200

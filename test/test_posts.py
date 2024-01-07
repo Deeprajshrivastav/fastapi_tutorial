2367faabfd96b59adffd8a91ef08e1b15d2edb77
deep132raj <deep@ai4mtech.com>
2024-01-07 10:23:17 +0530

    lastest_update

--- test/test_posts.py
+++ test/test_posts.py
@@ -1,4 +1,6 @@
 import pytest
+from app import schemas
+
 
 def test_all_posts(authorized_client, create_test_post):
     res = authorized_client.get('/post/')
@@ -28,3 +30,18 @@ def test_unauthorized_one_post(client, create_test_post):
     res = client.get(f'/post/{create_test_post[0].id}')
     assert res.status_code == 401
     assert res.json()['detail'] == 'Not authenticated'
+    
+@pytest.mark.parametrize('title, content, published', [('title 1', 'content 1', True),
+                                                       ('title 2', 'content 2', False),
+                                                       ('title 3', 'content 3', True),
+                                                       ('title 4', 'content 4', True),
+                                                       ('title 5', 'content 4', False)])
+def test_post(authorized_client, title, content, published):
+    response = authorized_client.post('/post', json={'title': title, 'content': content, 'published': published})
+    
+    assert response.status_code == 201
+    created_post = schemas.Post(**response.json())
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.published == published
+    
